fb59d2cd8bd95e152d7542656f6bfc702a3331cb
Rublov Viacheslav <timmy31101994@gmail.com>
2022-06-19 22:55:08 +0300

    HW_15

---
+++ Models/AsynchronousMethods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_15
+{
+    public class AsynchronousMethods
+    {
+        private string _helloFile = @"C:\Users\Timmy\OneDrive\Рабочий стол\A-Level\Новая папка\HW_15\Files\Hello.txt";
+        private string _worldFile = @"C:\Users\Timmy\OneDrive\Рабочий стол\A-Level\Новая папка\HW_15\Files\World.txt";
+
+        public string? Hello { get; set; }
+        public string? World { get; set; }
+
+        public async void ReadHelloAsync()
+        {
+            Hello = await File.ReadAllTextAsync(_helloFile);
+        }
+
+        public async void ReadWorldAsync()
+        {
+            World = await File.ReadAllTextAsync(_worldFile);
+        }
+
+        public async void Display()
+        {
+            ReadHelloAsync();
+            ReadWorldAsync();
+            await Task.Run(async () =>
+            {
+                await Task.Delay(1000);
+                Console.WriteLine(Hello + " " + World);
+            });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HW_15
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Starter starter = new Starter();
+            starter.Run();
+            Console.ReadKey();
+        }
+    }
+}
